f0723af5cc850ed6fa1d5ea0d96d6b9561303939
Siddhartha <siddharthasahu.work@gmail.com>
2021-11-16 21:47:37 +0530

    feat: translation functionality

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ var inputText = document.querySelector("#input-txt");
 var btnSubmit = document.querySelector("#btn-submit");
 var outputText = document.querySelector("#output-txt");
 
-function submitEventHandler(){
-    var input = inputText.value;
-    
-    var output = input + " is awsome 💯."
+function errorHandler(error){
+    console.log("Error occured", error);
+    alert("Server not responding");
+}
 
-    outputText.innerText = output.toUpperCase();    
+function submitEventHandler(){
+     var input = inputText.value;
+
+     var url = "https://api.funtranslations.com/translate/minion.json"
+     var serverURL = url + "?text=" + input;
+
+     fetch(serverURL)
+     .then(response => response.json())
+     .then(json => {
+         var translatedText = json.contents.translated;
+         outputText.innerText = translatedText;
+     })
+     .catch(errorHandler);
 }
 
 
